3d95ff33ca763a5a6733b4dc716f01cd5e2e7c87
Ashutosh Das <dasashutosh883@gmail.com>
2024-05-23 15:52:53 +0530

    Initial Commit

---
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleAPI.Controllers
+{
+    [ApiController, Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "Les Jackson";
+        }
+    }
+}
