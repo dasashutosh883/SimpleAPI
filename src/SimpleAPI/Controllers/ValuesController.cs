952fcc6d414a4503909807a0d907f884b353269b
Ashutosh Das <dasashutosh883@gmail.com>
2024-07-16 15:48:35 +0530

    changes in values controller

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -8,7 +8,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Les Jackson";
+            return "hello , G Gobinda Whats up";
         }
     }
 }
