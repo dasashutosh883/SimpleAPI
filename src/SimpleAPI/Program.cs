6f39858d25bfe4d873c8603b4b97471284f63e4d
Ashutosh Das <dasashutosh883@gmail.com>
2024-07-24 15:23:16 +0530

    Logging added with Serilog

--- src/SimpleAPI/Program.cs
+++ src/SimpleAPI/Program.cs
@@ -1,7 +1,9 @@
+using SimpleAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Host.SerilogConfig();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ src/SimpleAPI/Services/AppExtension.cs
@@ -0,0 +1,16 @@
+using Serilog;
+using Serilog.Formatting.Json;
+namespace SimpleAPI.Services
+{
+    public static class AppExtension
+    {
+        public static void SerilogConfig(this IHostBuilder host)
+        {
+            host.UseSerilog(configureLogger:(ContextBoundObject,loggerConfig)=>
+            {
+                loggerConfig.WriteTo.Console();
+                loggerConfig.WriteTo.File(new JsonFormatter(),path:"Logs/applogs-.text",rollingInterval:RollingInterval.Day);
+            });
+        }
+    }
+}
